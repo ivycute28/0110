42e6c0761d04c3d6eb5b5edfb15da3ef1848814b
嘻嘻哈哈三角形\hwliv <hwlivy@gmail.com>
2024-01-10 16:12:41 +0800

    透過自訂函數合併

--- 0110.R
+++ 0110.R
@@ -1,3 +1,8 @@
-dat1 = read.csv("data2_1.csv", header = TRUE, fileEncoding = 'CP950')
-dat1.clean = dat1[duplicated(dat1) == FALSE,]
-dat1.clean
+data_pipeline = function (file.path) {
+  dat1 = read.csv(file.path, header = TRUE, fileEncoding = 'CP950')
+  dat1.clean = dat1[duplicated(dat1) == FALSE,]
+  dat1.clean
+}
+
+data1_clean <- data_pipeline("data2_1.csv")
+head(data1_clean)
