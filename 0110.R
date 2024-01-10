dda3ac3160ba0e45bda6cbbced0b2d4ae05efd18
嘻嘻哈哈三角形\hwliv <hwlivy@gmail.com>
2024-01-10 16:18:11 +0800

    使用迴圈合併檔案

--- 0110.R
+++ 0110.R
@@ -21,3 +21,18 @@ data1.simple = data1_clean[,c("s_d0", "school")]
 data2.simple = data2_clean[,c("s_d0", "school")]
 simple.merge.dat = merge(data1.simple, data2.simple, by = "school", all = TRUE)
 head(simple.merge.dat)
+
+for (i in 1:2) {
+  if (i == 1) {
+    data_clean.start = data_pipeline(paste("data2_", i, ".csv", sep = ""))
+    data.simple.start = data_clean.start[,c("s_d0", "school")]
+  } else {
+    data_clean.continue = data_pipeline(paste("data2_", i, ".csv", sep = ""))
+    data.simple.continue = data_clean.continue[,c("s_d0", "school")]
+    data.simple.start = merge(data.simple.start, data.simple.continue, by = "school", all = TRUE)
+  }
+}
+
+colnames(data.simple.start) = c('school', paste('s_d0.', 1:2, sep = ""))
+
+head(data.simple.start)
